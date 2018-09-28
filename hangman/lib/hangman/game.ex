7718473506a75279bb8b7aaead20c9f8f06df771
SeungKiLee <seungkil@smu.edu>
2018-09-28 13:19:03 -0500

    utility functions for make_move

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,45 +1,74 @@
 defmodule Hangman.Game do
 
   defstruct(
-    game_state:      :initializing,
-    turns_left:      7,
-    word:   [],
-    guessed: []
+    game_state: :initializing,
+    turns_left: 7,
+    letters:    [],
+    used:       [],
+    last_guess: ""
   )
 
   # API
 
   def new_game() do
     new_word = new_word()
+
     %Hangman.Game{
-      word: new_word
+      letters: new_word
     }
   end
 
   def tally(game) do
-
-    displayed = show_letter(game.word, game.guessed)
+    displayed = show_letter(game.letters, game.used)
 
     %Hangman.Game{
       game_state: game.game_state,
       turns_left: game.turns_left,
-      guessed: game.guessed,
-      word: displayed
+      letters:    displayed,
+      used:       Enum.sort(game.used)
+      last_guess: game.last_guess
     }
   end
 
-  # Utility Functions
+  # Check if the guess is correct
+  # update the game state
+  def make_move(game, guess) do
 
+  end
+
+  '''
+  Specific
+          -> won
+          -> lost
+          -> already used
+          -> good guess
+  General -> bad guess
+  '''
+
+
+  # Utility Functions
   defp new_word() do
     Dictionary.random_word()
   end
 
-  defp show_letter(word, guessed) do
-    String.codepoints(word)
-    |> Enum.map( &display &1, &1 in guessed )
+  defp show_letter(letters, used) do
+    String.codepoints(letters)
+    |> Enum.map( &display &1, &1 in used )
   end
 
   defp display(letter, true), do: letter
   defp display(_, false),     do: "_"
+
+  defp add_guess_to_used(game, guess) do
+    %Hangman.Game{
+      game.used : Enum.sort([guess | game.used])
+    }
+  end
+
+  defp set_last_guess(game, guess) do
+    %Hangman.Game{
+      game.last_guess : guess
+    }
+  end
 
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -14,29 +14,32 @@ defmodule HangmanTest do
 
   test "New Game Word to Guess Exists" do
     new_game = Hangman.new_game()
-    assert String.length(new_game.word) > 0
+    assert String.length(new_game.letters) > 0
   end
 
   test "New Game Letters Guessed is Empty" do
     new_game = Hangman.new_game()
-    assert new_game.guessed == []
+    assert new_game.used == []
   end
 
   test "Tally Updated Correctly" do
     current_state = %Hangman.Game{
       game_state: :Drinking,
       turns_left: 6,
-      word:       "balmer",
-      guessed:    ["a", "b", "c"]
+      letters:    "balmer",
+      used:       Enum.sort (["b", "a", "c"]),
+      last_guess: "c"
     }
 
     return_state = %Hangman.Game{
       game_state: :Drinking,
       turns_left: 6,
-      word:       ["b", "a", "_", "_", "_", "_"],
-      guessed:    ["a", "b", "c"]
+      letters:    ["b", "a", "_", "_", "_", "_"],
+      used:       ["a", "b", "c"],
+      last_guess: "c"
     }
 
     assert Hangman.tally(current_state) == return_state
   end
+
 end
